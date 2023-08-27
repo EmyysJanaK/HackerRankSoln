4d55ccffe36b11ab951cfe200aed76619ef5f540
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-08-27 21:45:50 +0530

    SOLVED

--- SQL/Basic Select/Employee-Names.sql
+++ SQL/Basic Select/Employee-Names.sql
@@ -1,1 +1,6 @@
-select name from employee order by name;
+-- This SQL query retrieves the names of all employees from the EMPLOYEE table
+-- and orders them in ascending order.
+
+SELECT NAME 
+FROM EMPLOYEE
+ORDER BY NAME;
