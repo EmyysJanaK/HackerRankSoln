8af3a26d592dea614be9711cae262b197bb74e39
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-08-28 21:45:50 +0530

    SOLVED

--- SQL/Basic Select/Higher-Than-75-Marks.sql
+++ SQL/Basic Select/Higher-Than-75-Marks.sql
@@ -1,1 +1,8 @@
-select name from students where marks > 75 order by right(name,3),id asc;
+-- This SQL query selects the names of students who have marks greater than 75,
+-- and orders them by the last three characters of their names in ascending order,
+-- followed by their IDs in ascending order.
+
+SELECT NAME
+FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY RIGHT(NAME, 3), ID ASC;
