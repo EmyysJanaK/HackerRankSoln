a77794a4c8b92dd8c9102705f77bdb93337c1a3b
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-08-29 21:45:50 +0530

    SOLVED

--- SQL/Basic Select/Japanese-Cities'-Names.sql
+++ SQL/Basic Select/Japanese-Cities'-Names.sql
@@ -1,1 +1,5 @@
-select name from city where CountryCode="JPN";
+-- This SQL query retrieves the names of cities located in Japan.
+-- It selects the NAME column from the CITY table
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = "JPN";
