2ebc747274f3804fe0a86dcac16b25100df46c49
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-08-29 21:45:50 +0530

    SOLVED

--- SQL/Basic Join/African Cities.sql
+++ SQL/Basic Join/African Cities.sql
@@ -1,2 +1,6 @@
-SELECT CITY.name 
-FROM CITY JOIN country on CITY.countrycode = country.code where country.continent = 'Africa';
+-- This SQL query retrieves the names of all cities located in African countries.
+-- It joins the CITY table with the COUNTRY table on the country code
+SELECT CITY.name
+FROM CITY
+JOIN COUNTRY ON CITY.countrycode = COUNTRY.code
+WHERE COUNTRY.continent = 'Africa';
--- SQL/Basic Select/Japanese-Cities'-Attributes.sql
+++ SQL/Basic Select/Japanese-Cities'-Attributes.sql
@@ -1,1 +1,5 @@
-select * from city where countrycode="JPN";
+-- This SQL query retrieves all columns from the CITY table
+-- where the COUNTRYCODE is "JPN", indicating cities in Japan.
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = "JPN";
