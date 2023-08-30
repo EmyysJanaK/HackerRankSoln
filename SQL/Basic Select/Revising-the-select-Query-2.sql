e818b2108ad4442ee0191bdcc49dd565b102f717
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-08-30 21:45:50 +0530

    SOLVED

--- SQL/Basic Select/Revising-the-select-Query-2.sql
+++ SQL/Basic Select/Revising-the-select-Query-2.sql
@@ -1,3 +1,5 @@
+-- Query to select the names of cities with a population greater than 120,000 in the USA
+-- This SQL query retrieves the names of cities located in the USA
 SELECT NAME
 FROM CITY
 WHERE POPULATION > 120000
