3f16393e00645777cc739e0e3f007322562d3b20
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-08-30 21:45:50 +0530

    SOLVED

--- SQL/Basic Select/Revising-the-select-Query-1.sql
+++ SQL/Basic Select/Revising-the-select-Query-1.sql
@@ -1,3 +1,6 @@
+-- This SQL query selects all columns from the CITY table
+-- where the POPULATION is greater than 100000 and the COUNTRYCODE is "USA".
+-- It filters the results to include only cities in the USA with a population exceeding 100,000.
 SELECT *
 FROM CITY
 WHERE POPULATION > 100000
