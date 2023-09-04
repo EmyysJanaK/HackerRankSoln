801cf7197a9ac151993be883223a60237090dbcb
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-09-04 21:45:50 +0530

    SOLVED

--- SQL/Basic Select/Weather-Observation-Station-3.sql
+++ SQL/Basic Select/Weather-Observation-Station-3.sql
@@ -1,1 +1,5 @@
-Select distinct city from station where ID%2=0;
+-- Query to find distinct cities from the STATION table where the ID is even
+-- This SQL query retrieves distinct city names from the STATION table
+SELECT DISTINCT CITY
+FROM STATION
+WHERE ID % 2 = 0;
