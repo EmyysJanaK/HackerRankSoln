81365225ffd5d39bc996975e8fc4dbfc0a993ac5
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-09-05 21:45:50 +0530

    SOLVED

--- SQL/Basic Select/Weather-Observation-Station-4.sql
+++ SQL/Basic Select/Weather-Observation-Station-4.sql
@@ -1,1 +1,5 @@
-select count(city) - count(distinct city) from station;
+-- Query to find the number of cities that have duplicate entries in the STATION table
+-- This SQL query counts the number of cities in the STATION table that have duplicate entries.
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION
+WHERE CITY IS NOT NULL;
