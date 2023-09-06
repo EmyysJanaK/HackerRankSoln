1b92c4dd369379f568de3f8dab58c5d3a2b733f7
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-09-06 21:45:50 +0530

    SOLVED

--- SQL/Basic Select/Weather-Observation-Station-5.sql
+++ SQL/Basic Select/Weather-Observation-Station-5.sql
@@ -1,2 +1,14 @@
-select city,length(city) from station order By length(city) asc, city asc limit 1;
-select distinct(City),length(city) from station order by length(city) desc, city asc limit 1;
+-- and limits the result to only one row.
+-- This SQL query retrieves the shortest city name from the STATION table.
+-- It selects the CITY and its length, orders the results by the length of the city name in ascending order,
+SELECT CITY, LENGTH(CITY)
+FROM STATION
+ORDER BY LENGTH(CITY) ASC, CITY ASC
+LIMIT 1;
+
+-- This SQL query retrieves the longest city name from the STATION table.
+-- It selects the CITY and its length, orders the results by the length of the city name in descending order,
+SELECT DISTINCT CITY, LENGTH(CITY)
+FROM STATION
+ORDER BY LENGTH(CITY) DESC, CITY ASC
+LIMIT 1;
