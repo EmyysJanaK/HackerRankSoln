5817bfd95dde72449c1ebf50f546db99c6a4816b
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-09-07 21:45:50 +0530

    SOLVED

--- SQL/Basic Select/Weather-Observation-Station-12.sql
+++ SQL/Basic Select/Weather-Observation-Station-12.sql
@@ -1,1 +1,5 @@
-select distinct city from station where left(city,1) not in('a','e','i','o','u') and right(city,1) not in('a','e','i','o','u');
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u')
+  AND RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
