76e29fb1dfc8a0d6711846859af1457cfe8e732f
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-09-22 21:45:50 +0530

    SOLVED

--- SQL/Aggregation/Weather Observation Station 13.sql
+++ SQL/Aggregation/Weather Observation Station 13.sql
@@ -1,3 +1,6 @@
-select Round(sum(LAT_N),4)
-from STATION 
-where LAT_N > 38.7880 and LAT_N < 137.2345;
+-- Query to find the sum of LAT_N for stations within a specific latitude range
+-- This SQL query calculates the sum of LAT_N for all stations
+-- that have a latitude between 38.7880 and 137.2345.
+SELECT ROUND(SUM(LAT_N), 4)
+FROM STATION
+WHERE LAT_N > 38.7880 AND LAT_N < 137.2345;
