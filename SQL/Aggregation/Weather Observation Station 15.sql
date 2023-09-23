75a6674b190e19c7d452e1d1d3ac67b61a9236ed
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-09-23 21:45:50 +0530

    SOLVED

--- SQL/Aggregation/Weather Observation Station 15.sql
+++ SQL/Aggregation/Weather Observation Station 15.sql
@@ -1,3 +1,6 @@
- select Round(LONG_W,4)
- from  STATION
- where LAT_N = (Select Max(LAT_N)from STATION where LAT_N < 137.2345);
+-- Query to find the longitude of the station with the highest latitude less than 137.2345
+-- This SQL query retrieves the longitude (LONG_W) of the station
+-- that has the highest latitude (LAT_N) which is less than 137.2345.
+SELECT ROUND(LONG_W, 4)
+FROM STATION
+WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
