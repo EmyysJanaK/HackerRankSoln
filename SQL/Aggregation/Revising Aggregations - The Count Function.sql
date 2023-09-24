5825d7f80dc9d2e22c4a7adbf8429894fb085688
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-09-24 21:45:50 +0530

    SOLVED

--- SQL/Aggregation/Revising Aggregations - The Count Function.sql
+++ SQL/Aggregation/Revising Aggregations - The Count Function.sql
@@ -1,1 +1,5 @@
-SELECT COUNT(*) FROM CITY WHERE POPULATION > 100000
+-- This SQL query counts the number of cities in the CITY table that have a population greater than 100,000.
+-- It retrieves the total count of such cities without any additional filtering.
+SELECT COUNT(*) 
+FROM CITY 
+WHERE POPULATION > 100000
