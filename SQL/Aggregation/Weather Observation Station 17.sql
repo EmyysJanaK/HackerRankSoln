37bc80b23ef61f8ad6fb6581be28a8b518884c16
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-09-25 21:45:50 +0530

    SOLVED

--- SQL/Aggregation/Weather Observation Station 17.sql
+++ SQL/Aggregation/Weather Observation Station 17.sql
@@ -1,6 +1,8 @@
-select Round(LONG_W,4)
-from STATION
-where LAT_N = (
-select MIN(LAT_N) 
-from STATION
-where LAT_N > 38.7780);
+-- Write a SQL query to find the longitude of the station with the lowest latitude that is greater than 38.7780.
+-- -- This SQL query retrieves the longitude (LONG_W) of the station
+SELECT ROUND(LONG_W, 4)
+FROM STATION
+WHERE LAT_N = (
+    SELECT MIN(LAT_N)
+    FROM STATION
+    WHERE LAT_N > 38.7780);
