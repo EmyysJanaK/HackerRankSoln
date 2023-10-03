5387637d4d046c8b7377c5b9857f706aeb066ef1
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-10-03 21:45:50 +0530

    SOLVED

--- SQL/Basic Join/Contest Leaderboard.sql
+++ SQL/Basic Join/Contest Leaderboard.sql
@@ -1,7 +1,13 @@
-SELECT h.hacker_id, h.name, SUM(MAX_SCORE.t1) as total_score
-FROM Hackers h inner join 
+-- Contest Leaderboard
+-- This query retrieves the leaderboard for a coding contest, showing each hacker's total score
+-- based on their highest submission scores for each challenge.
+-- It aggregates the scores by hacker and orders the results by total score in descending order,
+-- with ties broken by hacker ID in ascending order.
+SELECT h.hacker_id, h.name, SUM(MAX_SCORE.t1) AS total_score
+FROM Hackers h
+INNER JOIN
 (
-    SELECT MAX(s.score) as t1, s.hacker_id  
+    SELECT MAX(s.score) AS t1, s.hacker_id
     FROM Submissions s
     GROUP BY s.challenge_id, s.hacker_id
     HAVING t1 > 0
