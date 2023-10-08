947f28c9779e388cf42cc8109fa5a810517ee3ab
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-10-08 21:45:50 +0530

    SOLVED

--- SQL/Advanced Select/Binary Tree Nodes.sql
+++ SQL/Advanced Select/Binary Tree Nodes.sql
@@ -1,4 +1,6 @@
-
+-- This SQL query classifies nodes in a binary search tree (BST) as 'Root', 'Inner', or 'Leaf'.
+-- It selects the node value and its classification based on its parent-child relationship.
+-- The 'Root' node has no parent, 'Inner' nodes have children, and 'Leaf' nodes have no children.
 SELECT BT.N,
 CASE
     WHEN BT.P IS NULL THEN 'Root'
