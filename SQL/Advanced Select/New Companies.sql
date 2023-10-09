af4c438b240971852852111f992323a39ea0af19
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-10-09 21:45:50 +0530

    SOLVED

--- SQL/Advanced Select/New Companies.sql
+++ SQL/Advanced Select/New Companies.sql
@@ -1,3 +1,7 @@
+-- This SQL query retrieves the number of distinct employees, managers, senior managers, and lead managers
+-- for each company, along with the company code and founder.
+-- It joins the Company, Lead_Manager, Senior_Manager, Manager, and Employee tables
+-- based on their respective codes and filters the results to group by company code and founder.
 SELECT c.company_code,c.founder,
 count(distinct lm.lead_manager_code),
 count(distinct sm.senior_manager_code),
