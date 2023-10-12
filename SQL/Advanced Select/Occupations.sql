b22e9d2e33cd85502597c4a5e5c0dd08463ef391
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-10-12 21:45:50 +0530

    SOLVED

--- SQL/Advanced Select/Occupations.sql
+++ SQL/Advanced Select/Occupations.sql
@@ -1,3 +1,6 @@
+-- This SQL query retrieves the names of individuals grouped by their occupations.
+-- It uses conditional aggregation to pivot the data, showing each occupation in a separate column.
+-- The query uses a subquery to assign row numbers within each occupation group, allowing for the selection of the first name in each group.
 SELECT 
     MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) AS Doctor,
     MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS Professor,
