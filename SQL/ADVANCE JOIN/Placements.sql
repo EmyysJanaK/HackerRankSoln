49dbab69a791b6bc78b40d65aad3ea4cf18c8a41
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-10-18 21:45:50 +0530

    SOLVED

--- SQL/ADVANCE JOIN/Placements.sql
+++ SQL/ADVANCE JOIN/Placements.sql
@@ -1,9 +1,14 @@
-Select S.Name
-from Students S inner join Friends f
-on S.ID = f.ID
-inner join Packages p
-on f.ID = p.ID
-inner join Packages fp
-on f.Friend_ID = fp.ID
-where fp.Salary > p.Salary
-order by fp.Salary;
+-- Find the names of students who have friends with a higher salary than their own package salary.
+-- This SQL query retrieves the names of students who have friends with a higher salary than their own package salary.
+-- It uses inner joins to connect the Students, Friends, and Packages tables.
+-- The query filters the results to include only those students whose friends' salaries are greater than their own package salaries.
+
+SELECT S.Name
+FROM Students S INNER JOIN Friends F
+ON S.ID = F.ID
+INNER JOIN Packages P
+ON F.ID = P.ID
+INNER JOIN Packages FP
+ON F.Friend_ID = FP.ID
+WHERE FP.Salary > P.Salary
+ORDER BY FP.Salary;
