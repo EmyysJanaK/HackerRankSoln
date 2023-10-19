9dce36384ad3c8f401413cfa821bf57b8c0eb724
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2023-10-19 21:45:50 +0530

    SOLVED

--- SQL/Aggregation/Weather Observation Station 16.sql
+++ SQL/Aggregation/Weather Observation Station 16.sql
@@ -1,5 +1,6 @@
 -- Query to find the minimum latitude of stations with latitude greater than 38.7780        
 -- This SQL query retrieves the minimum latitude (LAT_N) from the STATION table
+
 SELECT ROUND(MIN(LAT_N), 4) 
 FROM STATION
 WHERE LAT_N > 38.7780;
